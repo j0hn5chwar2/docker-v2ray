938adb9c3a595f2574b7b839406e87fe9c9d16cb
KC-C <rotweisskc@outlook.com>
2019-06-12 14:54:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ sed -i "s/\[VMESS_ID\]/${VMESS_ID}/g" /etc/v2ray/config.json
 sed -i "s/\[VMESS_LEVEL\]/${VMESS_LEVEL}/g" /etc/v2ray/config.json
 sed -i "s/\[VMESS_ALTERID\]/${VMESS_ALTERID}/g" /etc/v2ray/config.json
 sed -i "s/\[KCP_PORT_VMESS\]/${KCP_PORT_VMESS}/g" /etc/v2ray/config.json
-sed -i "s/\[KCP_MUT\]/${KCP_MUT}/g" /etc/v2ray/config.json
+sed -i "s/\[KCP_MTU\]/${KCP_MTU}/g" /etc/v2ray/config.json
 sed -i "s/\[KCP_TTI\]/${KCP_TTI}/g" /etc/v2ray/config.json
 sed -i "s/\[KCP_UPLINK\]/${KCP_UPLINK}/g" /etc/v2ray/config.json
 sed -i "s/\[KCP_DOWNLINK\]/${KCP_DOWNLINK}/g" /etc/v2ray/config.json
